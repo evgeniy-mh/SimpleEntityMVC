c5e864ad2cd612e7d7a9da48d470a308a75fc78e
evgeniy-mh <djpon94@gmail.com>
2017-06-27 19:10:44 +0300

    Data Access Layer

---
+++ MyUniversity/DataAccessLayer/SchoolContext.cs
@@ -0,0 +1,31 @@
+﻿using MyUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace MyUniversity.DataAccessLayer
+{
+    public class SchoolContext:DbContext
+    {
+        //The name of the connection string is passed in to the constructor. (Web.config)
+        public SchoolContext(): base("SchoolContext")
+        {
+
+        }
+
+        /* DbSet property for each entity set. 
+         * Entity set typically corresponds to a database table, and an entity corresponds to a row in the table.
+         */
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
