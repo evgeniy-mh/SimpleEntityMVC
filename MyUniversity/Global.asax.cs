eac189f1ffdf2b45781bdd83417ce783c9fa3d3a
evgeniy-mh <djpon94@gmail.com>
2017-06-30 19:42:52 +0300

    enable query logging and query interception

--- MyUniversity/Global.asax.cs
+++ MyUniversity/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using MyUniversity.DataAccessLayer;
+using System.Data.Entity.Infrastructure.Interception;
 
 namespace MyUniversity
 {
@@ -16,6 +18,8 @@ namespace MyUniversity
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            DbInterception.Add(new SchoolInterceptorLogging());
         }
     }
 }
